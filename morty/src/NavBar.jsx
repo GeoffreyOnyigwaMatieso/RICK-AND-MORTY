059cdeeb2af73ed8b3dd9fa834e1082fc9771c50
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-04 12:39:18 +0300

    :recycle:  Code Refactor our NavBar

--- morty/src/NavBar.jsx
+++ morty/src/NavBar.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
+import { NavLink} from "react-router-dom"
+
+//styling our NavBar 
+
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+
+      <NavLink
+        to="/profile"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Profile
+      </NavLink>
+
+      <NavLink
+        to="/seasons"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Seasons 
+      </NavLink>
+
+      <NavLink
+        to="/webshop"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Webshop 
+      </NavLink>
+    </div>
   )
 }
 
