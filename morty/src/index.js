5abe1c9375788236255b7ee45d86e7e409dde07d
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-05 09:17:34 +0300

    :recycle:  Code Refactoring

--- morty/src/index.js
+++ morty/src/index.js
@@ -2,7 +2,9 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./components/App";
+import "./index.css";
+
 
 ReactDOM.render(
   <BrowserRouter>
