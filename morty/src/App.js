2e343da3e5f5c78e39437b922b8eb51607caf9c7
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-06 11:26:58 +0300

    :wrench: Project Setup Complete

--- morty/src/App.js
+++ morty/src/App.js
@@ -3,7 +3,9 @@ import "bootstrap/dist/js/bootstrap"
 function App() {
   return (
     <div className="App">
-     <h1 className="text-center">Rick & Morty </h1>  
+     <h1 className="text-center ubuntu my-4">
+      Rick & Morty  <span className="text-primary"> Webpage </span> 
+     </h1>  
     </div>
   );
 }
