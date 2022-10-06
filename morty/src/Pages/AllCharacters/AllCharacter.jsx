716e07e0f06ae3cfec71b516910e6038a71fb389
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-06 04:24:05 +0300

    :construction:  set up useState and useEffect

--- morty/src/Pages/AllCharacters/AllCharacter.jsx
+++ morty/src/Pages/AllCharacters/AllCharacter.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect}from "react";
 
 function AllCharacter() {
+  
+  const [characterList ,setCharacterList] = useState([]);
+
+
+  // fetching data from the api
+  useEffect(()=>{
+    fetch("https://rickandmortyapi.com/api/character")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+
+  },[])
   return (
     <div>
       <h1> Main Characters! </h1>
