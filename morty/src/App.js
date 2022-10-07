9205b93bc7701e32851868c9f2de5255cd5ac4e0
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-07 09:40:28 +0300

    :construction: manipulation of App and NavBar file

--- morty/src/App.js
+++ morty/src/App.js
@@ -1,11 +1,28 @@
 import React  from 'react'
+import { Route, Switch } from "react-router-dom";
 import AllCharacter from './Components/AllCharacterContainer/AllCharacter';
+import Home from './Components/Home/Home';
+import { CharacterList } from './Components/CharacterList/CharacterList';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
 
   return (
     <div className="App">
-     <AllCharacter />
+    <NavBar />
+    <Switch>
+    <Route exact path ="/characterhint">
+    <AllCharacter />
+    </Route>
+
+    <Route exact path ="/allcharacter">
+    <CharacterList />
+    </Route>
+
+    <Route exact path ="/">
+    <Home />
+    </Route>
+    </Switch> 
     </div>
   );   
 }
--- morty/src/Components/NavBar/NavBar.jsx
+++ morty/src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
+import { NavLink} from "react-router-dom"
+
+//styling our NavBar 
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+
+      <NavLink
+        to="/allcharacter"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        CharacterList 
+      </NavLink>
+
+      <NavLink
+        to="/characterhint"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Character Hint 
+      </NavLink>
+    </div>
+   
   )
 }
 
 export default NavBar
+
+
+
+
+
+
+
+
