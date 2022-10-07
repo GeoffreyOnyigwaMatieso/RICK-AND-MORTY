dc0f9306fe7ec6ce8b5fff6e8a7a84dfcc52e2d0
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-07 06:13:29 +0300

    :wrench: CharacterList End

--- morty/src/Components/CharacterList/CharacterList.js
+++ morty/src/Components/CharacterList/CharacterList.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import  Character from '../Characters/Character'
 
-export const CharacterList = () => {
+// This component Displays the Character in form of a list 
+
+/*
+map the data coming from the Character in the table
+hold the mapped data in a characterList variable 
+*/
+
+export const CharacterList = ({Characters}) => {
+    // map data from character in the table store them in a variable characterlist variable
+    const characterList = Characters.map(item=>{
+        return <Character key={item.id} item = {item}/>
+    })
+
   return (
-    <div>CharacterList</div>
+    <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
+          <th>
+            <h3 className="ui center aligned header">name</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">status</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">LastKnownLocation</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Firstseenin</h3>
+          </th>
+        </tr>
+        {characterList}
+      </tbody>
+    </table>
   )
 }
