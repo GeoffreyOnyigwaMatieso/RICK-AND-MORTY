acae1435b4c1365f1d386e4f8252dfb9ed11ea95
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-07 07:42:45 +0300

    :wrench: Implementing Character container end

--- morty/src/Components/CharacterContainer/charactercontainer.js
+++ morty/src/Components/CharacterContainer/charactercontainer.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect}from "react";
+import { CharacterList } from "../CharacterList/CharacterList";
+import { Search } from "../Search/Search";
 
-export const charactercontainer = () => {
+
+
+ function CharacterContainer() {
+    const [Characters, setCharacters] = useState([]);
+    const [search, setSearch] = useState("")
+
+    useEffect(() =>{
+        const lookup = () =>{
+          fetch(`http://localhost:8001/charactersprofile?q=${search}`)
+          .then(resp => resp.json())
+          .then(data => setCharacters(data))
+        };
+        if(search.length === 0 || search.length > 2) lookup();
+      },[search])
+
   return (
-    <div>charactercontainer</div>
+    <div>
+      <Search search = {setSearch}/>
+      
+      <CharacterList Characters = {Characters}/>
+    </div>
   )
 }
+
+export default CharacterContainer
