64a0b449c92ad37c71fa206be7af123d9b6ae202
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-07 19:13:04 +0300

    :wrench: Implemented AddCharacterForm

--- morty/src/Components/AddCharacterForm/AddCharacterForm.js
+++ morty/src/Components/AddCharacterForm/AddCharacterForm.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, {useState} from "react";
 
-function AddCharacterForm() {
+function AddCharacterForm({charaterForm}) {
+    const [name, setName] = useState("");
+    const [status, setStatus] = useState("");
+    const [LastKnownLocation, setLastKnownLocation] = useState("");
+    const [Firstseenin, setFirstseenin] = useState("")
+
+    function handleSubmit(e){
+        const charItem = {
+            name:name,
+            status:status,
+            LastKnownLocation:LastKnownLocation,
+            Firstseenin:Firstseenin
+        }
+        post(charItem)
+    }
+
+    function post (data){
+        fetch("http://localhost:8001/transactions", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data)
+        })
+        charaterForm(data)
+      }
+
   return (
-    <div>AddCharacterForm</div>
+    <div className="ui segment">
+    <form className="ui form" onSubmit = {handleSubmit}>
+      <div className="inline fields">
+        <input type="text" name="name" value = {name} onChange ={(e) =>setName(e.target.value)}/>
+        <input type="text" name="status" placeholder="status" value = {status} onChange={(e)=>setStatus(e.target.value)} />
+        <input type="text" name="LastKnownLocation" placeholder="LastKnownLocation" value = {LastKnownLocation} onChange={(e) => setLastKnownLocation(e.target.value)}/> 
+        <input type="number" name="Firstseenin" placeholder="Firstseenin"  value = {Firstseenin} onChange={(e) => setFirstseenin(e.target.value)}/>
+      </div>
+      <button type = "submit" className="ui button">
+        Add Character
+      </button>
+    </form>
+  </div>
   )
 }
 
