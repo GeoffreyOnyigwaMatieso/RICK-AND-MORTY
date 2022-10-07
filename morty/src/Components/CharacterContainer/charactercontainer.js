375e1ea69a14cb8cdd21b89384caf4306bd4f017
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-07 18:24:11 +0300

    :wrench: Removed Space in db.json file

--- morty/src/Components/CharacterContainer/charactercontainer.js
+++ morty/src/Components/CharacterContainer/charactercontainer.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect}from "react";
-// import { CharacterList } from "../CharacterList/CharacterList";
 import { Search } from "../Search/Search";
 
 import CharacterList from "../CharacterList/CharacterList";
