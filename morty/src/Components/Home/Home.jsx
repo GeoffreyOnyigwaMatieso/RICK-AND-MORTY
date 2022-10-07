3cafbb4c1693c556be88d2a89ae12b4aa2a9cadf
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-07 09:09:25 +0300

    :wrench: Implemented Home and  created NavBar Component

--- morty/src/Components/Home/Home.jsx
+++ morty/src/Components/Home/Home.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="welcome">
+                <div className="welcomeText">
+                <h1>Rick And Morty </h1>
+                    <p> Rick and Morty is an American adult animated science <br />fiction sitcom created by Justin Roiland and Dan 
+                     <br />Harmon for Cartoon Network's late night programming block Adult swim </p>
+                </div>
+                <div className="getstarted">
+                    <h2>I love Rick and Morty </h2>
+                    <img src="./Assets/slide_1.png" alt=" " />   
+                </div>
+            </div>
+    </div>
   )
 }
 
 export default Home
+
+
