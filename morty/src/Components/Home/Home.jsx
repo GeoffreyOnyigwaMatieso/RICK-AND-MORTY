6b5db24014adfdb7ee8fab916e55a8c9a4e19516
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-09 17:28:05 +0300

    Style changes

--- morty/src/Components/Home/Home.jsx
+++ morty/src/Components/Home/Home.jsx
@@ -6,21 +6,19 @@ import './Home.css'
 function Home() {
   return (
     <div>
-      <div className="welcome">
-                <div className="welcome">
-                <h1 className="ubuntu my-4">Rick & <span className="text-primary">Morty</span> </h1>
-                <div className="getstarted">
+      <div className="getstarted">
                 <img src="Images/bgone.png" alt="" img-fluid/>    
+                </div>
+                <div class="text-center">
+                <img src="Images/logo.png" alt ="" img-fluid/>
                 </div>
                 <h1 className="ubuntu my-4">Rick & <span className="text-primary">Morty</span> </h1>
                     <span style={{color:'White'}}>
-                    <p className ="fs-6  fw-bold font-monospace" > Rick and Morty is an American adult animated science <br />fiction sitcom created by Justin Roiland and Dan 
+                    <p class="fs-6  fw-bold font-monospace" > Rick and Morty is an American adult animated science <br />fiction sitcom created by Justin Roiland and Dan 
                      <br />Harmon for Cartoon Network's late night programming block Adult swim </p>
                     </span>
-                </div>
-              
             </div>
-    </div>
+    
   )
 }
 
