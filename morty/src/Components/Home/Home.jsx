42ce9e3acda716087f4085f97ddac0b1a8142d55
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-09 12:44:11 +0300

    styling our css

--- morty/src/Components/Home/Home.jsx
+++ morty/src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 function Home() {
   return (
     <div>
       <div className="welcome">
                 <div className="welcomeText">
-                <h1>Rick And Morty </h1>
+                <h1 className="ubuntu my-4">Rick & <span className="text-primary">Morty</span> </h1>
                     <p> Rick and Morty is an American adult animated science <br />fiction sitcom created by Justin Roiland and Dan 
                      <br />Harmon for Cartoon Network's late night programming block Adult swim </p>
                 </div>
