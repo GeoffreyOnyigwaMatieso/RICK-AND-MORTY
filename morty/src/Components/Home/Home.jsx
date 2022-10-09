abc32333e4ce7b15c5cb5d7e1f9ce9b38d351486
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-09 14:54:47 +0300

    Styling our web page

--- morty/src/Components/Home/Home.jsx
+++ morty/src/Components/Home/Home.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import './Home.css'
 
 function Home() {
   return (
     <div>
       <div className="welcome">
-                <div className="welcomeText">
+                <div className="welcome">
                 <h1 className="ubuntu my-4">Rick & <span className="text-primary">Morty</span> </h1>
-                    <p> Rick and Morty is an American adult animated science <br />fiction sitcom created by Justin Roiland and Dan 
-                     <br />Harmon for Cartoon Network's late night programming block Adult swim </p>
-                </div>
                 <div className="getstarted">
-                    <h2>I love Rick and Morty </h2>
-                    <img src="./Assets/slide_1.png" alt=" " />   
+                <img src="Images/bgone.png" alt="" img-fluid/>    
                 </div>
+                <h1 className="ubuntu my-4">Rick & <span className="text-primary">Morty</span> </h1>
+                    <span style={{color:'White'}}>
+                    <p className ="fs-6  fw-bold font-monospace" > Rick and Morty is an American adult animated science <br />fiction sitcom created by Justin Roiland and Dan 
+                     <br />Harmon for Cartoon Network's late night programming block Adult swim </p>
+                    </span>
+                </div>
+              
             </div>
     </div>
   )
